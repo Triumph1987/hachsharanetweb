831107b17969369f7339573737e2977d1180d4fa
sys16-4 <sys16-4@303-4>
2017-06-06 20:49:21 +0300

    menu change

--- hachsharanetweb/General.Master.designer.cs
+++ hachsharanetweb/General.Master.designer.cs
@@ -31,15 +31,6 @@ namespace hachsharanetweb {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
-        /// ContentPlaceHolder4 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder4;
-        
-        /// <summary>
         /// ContentPlaceHolder3 control.
         /// </summary>
         /// <remarks>
@@ -47,6 +38,15 @@ namespace hachsharanetweb {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder3;
+        
+        /// <summary>
+        /// ContentPlaceHolder1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
         
         /// <summary>
         /// form1 control.
